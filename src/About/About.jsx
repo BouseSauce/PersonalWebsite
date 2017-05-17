6e938615516e45e9854adbe75a3486f1ad339e04
SamKennedy <sam.kennedy02@gmail.com>
2017-05-17 15:39:57 +1200

    Added About component

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -6,9 +6,7 @@ function template() {
   return (
     <div className="about">
       <h1>Understanding my passion</h1>
-   
-      	
-      	<p>There's something to be said about working with passionate business owners that truly love what they do. I’m no different. I started my business because I honestly enjoy coming to work every day to create something beautiful that has the ability to impact others. </p>
+      	<p>There's something to be said about working with beautiful ideas to turn them into impactful web pages. I started developement to create something beautiful that has the ability to impact others. </p>
     	<img src={MyFace} className="faceImage" />
     </div>
   );
--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -3,14 +3,17 @@ import React from "react";
 
 
 function template() {
+
+  var openBrace = '{';
+  var closeBrace = '}';
   return (
   	<div className="container">
 
     <div className="intro">
        
         <div className="introContent">
-    	<p className="subText">Resume, Github & Portfolio</p>
-      	<p className="heading">Web Developer</p>
+    	<p className="subText">Resume, Github & Portfolio </p>
+      	<p className="heading">{openBrace} Web Developer {closeBrace}</p>
       	  <div className="buttonContainer">
    			<a href="https://www.dropbox.com/s/81yyq3xqimgyfqs/Samuel%20Kennedy%20CV.pdf?dl=0"><h1 className="button">Resume</h1></a>
    			<a href='#'><h1 className="button">Portfolio</h1></a>
